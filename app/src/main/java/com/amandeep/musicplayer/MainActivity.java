5885b1412a268649bde1b87d8eb57d81aad94d01
Amandeep <amankumar283@gmail.com>
2023-06-20 14:48:38 -0400

    Widget Declared

--- app/src/main/java/com/amandeep/musicplayer/MainActivity.java
+++ app/src/main/java/com/amandeep/musicplayer/MainActivity.java
@@ -2,9 +2,21 @@ package com.amandeep.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button forward,rewind,play,pause;
+    TextView start,end;
+    SeekBar seekBar;
+    MediaPlayer mediaPlayer;
+    Handler handler=new Handler();
+    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
